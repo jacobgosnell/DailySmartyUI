371a64959ed32539869f532804b6f82157af4e0d
Jake <gosnelljacob@gmail.com>
2019-08-25 19:33:25 -0700

    setup results component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -10,6 +10,7 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 import "./style/main.scss";
 
 import Home from "./components/home";
+import Results from "./components/results";
 
 function main() {
   ReactDOM.render(
@@ -17,7 +18,7 @@ function main() {
       <BrowserRouter>
         <Switch>
           <Route path='/' component={Home}/>
-          <Route path='/results' component={Home} />
+          <Route path='/results' component={Results} />
         </Switch>
       </BrowserRouter>
     </Provider>,
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -5,6 +5,7 @@ class SearchBar extends Component {
 
   handleFormSubmit = function({query}) {
     console.log('trying to handle submit for query', query);
+    // navigate to a new route
   }
 
   renderInput(field) {

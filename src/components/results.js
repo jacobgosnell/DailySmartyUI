611a7169e7ccd5df0c650732181ed55e7c8450b5
Jake <gosnelljacob@gmail.com>
2019-08-27 20:02:28 -0700

    modified logo component to take in a custom size via props

--- src/components/results.js
+++ src/components/results.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
+import SearchBar from './searchbar';
 
 class Results extends Component {
   render() {
     return (
       <div>
-        <h1>Results</h1>
-        <Logo />
+        <Logo size={55}/>
+        <SearchBar />
       </div>
     )
   }

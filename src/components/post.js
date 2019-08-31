3e689b6e124a8c306c042f4c83fa6ba571ecc95b
Jake <gosnelljacob@gmail.com>
2019-08-30 17:26:50 -0700

    added react-animate-height npm package and used it to add hover animations to the post component with links

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,15 @@
 import react, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      height: 0
+    }
+  }
 
   renderTopics () {
     let topics = this.props.associated_topics.map((topic, index) => {
@@ -43,11 +52,22 @@ class Post extends Component {
             {this.renderTopics}
           </div>
           <div className="result-post__title">
-            {this.props.title}
+            <a 
+              href={this.props.url_for_post}
+              onMouseEnter={() => this.setState({ height: 70 })}
+              onMouseLeave={() => this.setState({ height: 0 })}
+            >
+              {this.props.link}
+            </a>
           </div>
-          <div className="result-post__links">
-            {this.renderLinks}
-          </div>
+          <AnimateHeight
+            duration={500}
+            height={this.state.height}
+          >
+            <div className="result-post__links">
+              {this.renderLinks}
+            </div>
+          </AnimateHeight>
         </li>
       )
     }
